6539b0e01dc0508460052b517f7f3f8a165e165e
Rajat Porwal <rajatporwal@protonmail.com>
2024-05-26 03:03:38 +0530

    add a div to wrap search box and button, add classes to elements in javascript for css layout

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
+import './style.css';
+
 const currentTemp = document.getElementById('currentTemp');
 const condition = document.getElementById('condition');
-const locationElem = document.getElementById('location'); 
+const locationElem = document.getElementById('location');
 const country = document.getElementById('country');
 const icon = document.getElementById('icon');
 const searchLocation = document.getElementById('search');
 const btn = document.getElementById('btn');
 const FahrenheitBtn = document.getElementById('cToFBtn');
+
+currentTemp.classList.add('temp');
+condition.classList.add('condition-text');
+locationElem.classList.add('location-text');
+country.classList.add('country-text');
+icon.classList.add('weather-icon');
+searchLocation.classList.add('search-input');
+btn.classList.add('search-button');
+FahrenheitBtn.classList.add('temp-toggle');
 
 function weatherFetcher(location) {
     return new Promise((resolve, reject) => {
@@ -39,7 +50,7 @@ function fahrenheitToCelsius(fahrenheit) {
 function displayWeather(response) {
     currentTemp.innerText = response.current.temp_c + '°C';
     condition.innerText = response.current.condition.text;
-    locationElem.innerText = response.location.name + ','; 
+    locationElem.innerText = response.location.name + ',';
     country.innerText = response.location.country;
     icon.src = 'https:' + response.current.condition.icon;
 }
@@ -54,7 +65,7 @@ function weatherDisplay() {
         });
 
     btn.addEventListener('click', () => {
-        const searchLocationValue = searchLocation.value; 
+        const searchLocationValue = searchLocation.value;
         weatherFetcher(searchLocationValue)
             .then(response => {
                 displayWeather(response);
@@ -65,20 +76,23 @@ function weatherDisplay() {
     });
 
     FahrenheitBtn.addEventListener('click', () => {
+        const tempText = currentTemp.innerText;
+        const tempValue = parseFloat(tempText);
+
         if (FahrenheitBtn.classList.contains('celsius')) {
-            const tempCelsius = parseFloat(currentTemp.innerText);
-            currentTemp.innerText = celsiusToFahrenheit(tempCelsius) + '°F';
+            currentTemp.innerText = celsiusToFahrenheit(tempValue).toFixed(1) + '°F';
             FahrenheitBtn.classList.remove('celsius');
             FahrenheitBtn.classList.add('fahrenheit');
             FahrenheitBtn.innerText = 'Convert to Celsius';
         } else {
-            const tempFahrenheit = parseFloat(currentTemp.innerText);
-            currentTemp.innerText = fahrenheitToCelsius(tempFahrenheit) + '°C';
+            currentTemp.innerText = fahrenheitToCelsius(tempValue).toFixed(1) + '°C';
             FahrenheitBtn.classList.remove('fahrenheit');
             FahrenheitBtn.classList.add('celsius');
             FahrenheitBtn.innerText = 'Convert to Fahrenheit';
         }
     });
+
+    
 }
 
 weatherDisplay();
